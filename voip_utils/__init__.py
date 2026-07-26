561ce047384f6b0135ab51ef0fb50371bc5df6d7
JulienDeveaux <julien.deveaux@hotmail.com>
2026-07-26 15:02:20 +0200

    Add intercom feature

--- voip_utils/__init__.py
+++ voip_utils/__init__.py
@@ -1,6 +1,7 @@
 """Voice over IP utilities."""
 
-from .sip import SIP_PORT, CallInfo, SdpInfo, SipDatagramProtocol
+from .call_bridge import BridgedCall, CallBridge, CallBridgeState
+from .sip import SIP_PORT, CallInfo, SdpInfo, SipDatagramProtocol, SipEndpoint, get_sip_endpoint
 from .voip import (
     RtcpDatagramProtocol,
     RtcpState,
